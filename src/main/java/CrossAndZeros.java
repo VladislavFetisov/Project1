18e0f152648bf2627382579b959e0a7eabc6cf7a
Владислав <cryptamyroadtothedream@yandex.ru>
2020-02-19 22:15:38 +0300

    Сделал все,кроме диагоналей и тестов

--- src/main/java/CrossAndZeros.java
+++ src/main/java/CrossAndZeros.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.*;
+
 public class CrossAndZeros {
     int size;
     char[][] table;
@@ -7,11 +9,45 @@ public class CrossAndZeros {
         this.table = new char[size][size];
     }
 
-    public void addSymbol(char symbol, int column, int line) {
-        this.table[column][line] = symbol;
+    public void addSymbol(char symbol, int line, int column) {
+        this.table[line][column] = symbol;
     }
 
-    public void clearCell(int column, int line) {
-        this.table[column][line] = ' ';
+    public void clearCell(int line, int column) {
+        this.table[line][column] = '*';
     }
+
+    public int longestLineOrCol(char symbol) {
+        int countI = 0, countJ = 0, maxCountI = 0, maxCountJ = 0;
+
+        for (int i = 0; i < this.table.length; i++) {
+            for (int j = 0; j < this.table.length; j++) {
+                if (this.table[i][j] == symbol) {
+                    countI++;
+                    if (countI > maxCountI) maxCountI = countI;
+                } else countI = 0;
+                if (this.table[j][i] == symbol) {
+                    countJ++;
+                    if (countJ > maxCountJ) maxCountJ = countJ;
+                } else countJ = 0;
+            }
+            countI = 0;
+            if (maxCountI == this.table.length || maxCountJ == this.table.length) break;
+        }
+        return max(maxCountI, maxCountJ);
+    }
+
+    //public int longestDiagonal(char symbol) { }
+
+    public void returnTable() {
+        for (char[] chars : this.table) {
+            for (int j = 0; j < this.table.length; j++) {
+                if (chars[j] != '0' && chars[j] != 'X') System.out.print('*');
+                else System.out.print(chars[j]);
+            }
+            System.out.print("\n");
+        }
+    }
+
 }
+
