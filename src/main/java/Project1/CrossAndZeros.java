2fabf74b99cff899fc28ba1c34e74eb1190e6f8c
Владислав <cryptamyroadtothedream@yandex.ru>
2020-03-10 16:36:09 +0300

    This code request checking,pls

--- src/main/java/Project1/CrossAndZeros.java
+++ src/main/java/Project1/CrossAndZeros.java
@@ -139,4 +139,3 @@ public class CrossAndZeros {
         return result;
     }
 }
-
--- src/main/java/Project1/Main.java
+++ src/main/java/Project1/Main.java
@@ -1,10 +1,7 @@
 package Project1;
 
-import java.util.Objects;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Objects.hash(4));
-        System.out.println(Objects.hash(3));
+
     }
 }
